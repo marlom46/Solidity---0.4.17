28ba4c765d920c554130b024aa79d43fd41fb549
marlom46 <80200977+marlom46@users.noreply.github.com>
2021-06-29 11:21:03 +0530

    Initial Commit
    New contract: Lottery

---
+++ lottery/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const mnemonicPhase = '--INSERT MNEMONIC HERE---';
+
+let provider = new HDWalletProvider({
+
+    mnemonic: mnemonicPhase,
+    providerOrUrl: 'https://rinkeby.infura.io/v3/717bf019dabe406495afdfecca0708d5'
+});
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account: ' + accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello There!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+    
+    console.log('Contract deployed to:' + result.options.address);
+};
+
+deploy();
